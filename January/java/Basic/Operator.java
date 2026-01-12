808810c4e47bb61629f02fab9f0ee2b8e5ce3e4c
Taksh1797 <takshbhavsar1797@gmail.com>
2026-01-12 23:32:59 +0530

    Update Operator.java
    I forgot about it before...
    Beginner mistake

--- January/java/Basic/Operator.java
+++ January/java/Basic/Operator.java
@@ -5,16 +5,34 @@ public class Operator {
         Scanner sc = new Scanner(System.in);
         int a,b;
 
-        //Ar
+        //Arithmetic Operators
 
         System.out.print("Enter First Number : ");
         a = sc.nextInt();
         System.out.print("Enter Second Number : ");
         b = sc.nextInt();
-        System.out.println("Sum of "+a+" & "+b+" : "+(a+b));
-        System.out.println("Min of "+a+" & "+b+" : "+(a-b));
-        System.out.println("Mul of "+a+" & "+b+" : "+(a*b));
-        System.out.println("Div of "+a+" & "+b+" : "+(a/b));
-        System.out.println("Mod of "+a+" & "+b+" : "+(a%b));
+        System.out.println(a+" + "+b+" : "+(a+b));
+        System.out.println(a+" - "+b+" : "+(a-b));
+        System.out.println(a+" * "+b+" : "+(a*b));
+        System.out.println(a+" / "+b+" : "+(a/b));
+        System.out.println(a+" % "+b+" : "+(a%b));
+
+        //Relational Operators
+
+        System.out.println(a+" > "+b+" : "+(a>b));
+        System.out.println(a+" < "+b+" : "+(a<b));
+        System.out.println(a+" = "+b+" : "+(a==b));
+        System.out.println(a+" != "+b+" : "+(a!=b));
+
+        //Bitwise Operators
+
+        System.out.println(a+" AND "+b+" : "+(a&b));
+        System.out.println(a+" OR "+b+" : "+(a|b));
+        System.out.println(a+" XOR "+b+" : "+(a^b));
+        System.out.println(a+" NOT "+b+" : "+(~b));
+        System.out.println(a+" Left Shift : "+(a<<2));
+        System.out.println(b+" Left Shift : "+(b<<2));
+        System.out.println(a+" Right Shift : "+(a>>2));
+        System.out.println(b+" Right Shift : "+(b>>2));
     }
 }
